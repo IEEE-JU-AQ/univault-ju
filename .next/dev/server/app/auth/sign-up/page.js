a8e9af7ba2eb1da100e71aca9e9494f3adeaa0e3
Zaid Qtaish <enderzaide@gmail.com>
2026-01-15 18:17:35 +0300

    fix: fixed tailwind and changed logo icon

--- .next/dev/server/app/auth/sign-up/page.js
+++ .next/dev/server/app/auth/sign-up/page.js
@@ -1,6 +1,6 @@
 var R=require("../../../chunks/ssr/[turbopack]_runtime.js")("server/app/auth/sign-up/page.js")
 R.c("server/chunks/ssr/node_modules_next_dist_58850a0a._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__099e2d6e._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__84acf1b4._.js")
 R.c("server/chunks/ssr/node_modules_next_dist_7381059c._.js")
 R.c("server/chunks/ssr/[externals]__7f148858._.js")
 R.c("server/chunks/ssr/node_modules_next_dist_client_components_builtin_global-error_ece394eb.js")
---
+++ components/ui/NavBar.tsx
@@ -0,0 +1,25 @@
+import { AuthButton } from "@/components/auth-components/auth-button";
+import { Suspense } from "react";
+import { Folder } from "lucide-react";
+import Link from "next/link";
+
+export default function NavBar() {
+    return (
+        <nav className="w-full border-b border-b-foreground/10">
+            <div className="max-w-5xl mx-auto flex justify-between items-center py-4">
+                <div className="flex gap-1"><Folder size={30} /><h1 className="text-2xl font-bold">UniVault</h1></div>
+
+                <div>
+                    <Link href="/gpa-calculator">
+                        <ul>
+                            <li className="hover:text-orange-200 list-none">GPA Calculator</li>
+                        </ul>
+                    </Link>
+                </div>
+                <Suspense>
+                    <AuthButton />
+                </Suspense>
+            </div>
+        </nav>
+    );
+}
