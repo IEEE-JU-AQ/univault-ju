9a50a4b900aaf6562204efccd97b0c17ca029b8e
Zaid Qtaish <enderzaide@gmail.com>
2026-01-17 20:27:29 +0300

    feat: made majors side bar into a seperate component and gave it an expandable state

--- .next/dev/static/chunks/app_page_tsx_332efca5._.js
+++ .next/dev/static/chunks/app_page_tsx_332efca5._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/node_modules_d37f6871._.js",
-  "static/chunks/_0c4846e8._.js"
+  "static/chunks/_2792fbda._.js"
 ],
     source: "dynamic"
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,13 @@
 import NavBar from "@/components/ui/NavBar";
 import CourseCard from "@/components/ui/CourseCard";
-import majors from "./data/majors.json";
+import MajorsSidebar from "@/components/ui/MajorsSidebar";
 
 export default function Home() {
-
-  const majorList = majors.map((faculty) => (
-    <div key={faculty.id} className="mb-6">
-      <h3 className="font-semibold text-lg mb-2">{faculty.faculty_name}</h3>
-      <ul className="space-y-1 ml-4">
-        {faculty.majors.map((major) => (
-          <li key={major.id} className="text-sm cursor-pointer hover:text-green-600">
-            {major.name}
-          </li>
-        ))}
-      </ul>
-    </div>
-  ))
   return (
     <>
       <NavBar />
       <main className="flex min-h-screen">
-        <div className="max-h w-70 border-2 border-t-0 border-l-0 p-4 overflow-y-auto">
-          {majorList}
-        </div>
+        <MajorsSidebar />
         <CourseCard /> <CourseCard /> <CourseCard /> <CourseCard />
       </main>
     </>
