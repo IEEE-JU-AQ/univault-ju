e7e6627e03174432f2d4529fe1780d769392d9f5
Zaid Qtaish <enderzaide@gmail.com>
2026-01-24 05:10:09 +0300

    feat: added major title to courses grid

--- app/courses/[majorId]/page.tsx
+++ app/courses/[majorId]/page.tsx
@@ -1,10 +1,11 @@
-import { getCourses } from "@/lib/majors";
+import { getCourses, getMajorfromId } from "@/lib/majors";
 import CourseGrid from "@/app/components/CourseGrid";
 
 export default async function majorCoursesPage({ params }: { params: Promise<{ majorId: string }> }) {
     const { majorId } = await params;
     const courses = getCourses(majorId);
+    const major = getMajorfromId(majorId);
     return (
-        <CourseGrid courses={courses} />
+        <CourseGrid courses={courses} majorName={major?.name} />
     );
 }
