6e5dfaa4352ad9789b91083dc97f0d278665743e
Zaid Qtaish <enderzaide@gmail.com>
2026-02-22 03:33:28 +0300

    chore: added no resource found in this category when theres no resources

---
+++ app/courses/course/[courseId]/[category]/page.tsx
@@ -0,0 +1,29 @@
+import ResourceCard from "@/app/components/ResourceCard";
+import { fetchResourcesByCourse } from "@/lib/utils/CourseUtils";
+
+export default async function ResourceCategoryPage({ params }: { params: Promise<{ courseId: string; category: string }> }) {
+const { category, courseId } = await params;
+const resources = await fetchResourcesByCourse(courseId);
+
+  return (
+    <main className="p-6 pb-10">
+      <h1 className="capitalize text-xl font-bold mb-3 mt-4">
+        {category.replace('-', ' ')}
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {resources
+          .filter((resource) => resource.category === category)
+          .sort((a, b) => b.stars - a.stars)
+          .map((resource) => (
+            <ResourceCard key={resource.id} {...resource} />
+          ))}
+      </div>
+
+      {resources.length === 0 && (
+        <div className="flex flex-col items-center gap-4 mt-10">
+          <p className="text-lg text-muted-foreground">No resources found in this category.</p>
+        </div>
+      )}
+    </main>
+  );
+}
