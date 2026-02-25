56be5eddf197fee2ca67b2a134104cb0c0e29fb0
Zaid Qtaish <enderzaide@gmail.com>
2026-02-25 23:17:08 +0300

    fix: fixed navbar alignmet on mobile

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -9,14 +9,14 @@ export default function NavBar() {
     const pathname = usePathname();
     return (
         <nav className="w-full border-b border-b-foreground/10 sticky top-0 z-50 bg-layout-background dark:bg-layout-background">
-            <div className="max-w flex justify-between items-center py-4 px-10">
+            <div className="max-w flex justify-between items-center py-4 px-4 sm:px-10">
                 <Link href="/" className="no-underline">
                     <div className="flex gap-1">
                         <Image src="/UJ.svg" alt="UJ Logo" width={30} height={30} />
                         <h1 className="text-2xl font-bold">UniVault</h1>
                     </div>
                 </Link>
-                <div className="flex gap-20 items-center">
+                <div className="flex gap-4 sm:gap-20 items-center">
                     <Link href="/gpa-calculator">
                         <ul>
                             <li className={`font-medium tracking-wide border-b-1 border-[var(--primary)] transition-all pb-1 ${pathname == "/gpa-calculator" ? "border-b-4" : ""}`}>GPA Calculator</li>
